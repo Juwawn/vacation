fd1783868aa05ddddae7ecb7d4157135d1ce3a69
juwawn <juwawnclark13@yahoo.com>
2024-05-07 15:31:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+const App =()=> {
+
+    return (
+
+        <>
+            <h1>Can You See ME??????</h1>
+        </>
+
+    )
+}
+
+export default App
---
+++ src/locations.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 1,
+        location: "Anguilla",
+        image: <img src="/images/anguilla.jpg"></img>
+    },
+    {
+        id: 2,
+        location: "Toyko",
+        image: <img src="/images/tokyo.jpg"></img>
+    },
+    {
+        id: 3,
+        location: "Ghana",
+        image: <img src="/images/ghana.jpg"></img>
+    },
+    {
+        id: 4,
+        location: "Hawaii",
+        image: <img src="/images/hawaii.jpg"></img>
+    },
+    {
+        id: 5,
+        location: "Barbados",
+        image: <img src="/images/barbados.jpg"></img>
+    }
+]
